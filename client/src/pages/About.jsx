a3087bcc011ecf526da7973e223e6df95953bed1
Saad Akbar <102979976+saadakbar1@users.noreply.github.com>
2024-07-06 16:55:01 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,11 +4,11 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Saad' Blog
+            About DevBytes
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Saad's Blog! This blog was created by Saad Akbar
+              Welcome to DevBytes! This blog was created by Saad Akbar
               as a personal project to share his thoughts and ideas with the
               world. I am a passionate  MERN stack developer and competitive programmer at Codeforces. I am good at on Data Structures and Algorithm. Loves to do photography.
             </p>
